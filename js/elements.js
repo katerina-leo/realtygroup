f3d7386a512dc3309bef33b31338045bf56c1051
katerina-leo <81039066+katerina-leo@users.noreply.github.com>
2024-01-26 11:25:18 +0000

    Deploying to gh-pages from @ katerina-leo/realtygroup@7702e8b8c990a68ab3ac280d8a0f96131a96b56e 🚀

--- js/elements.js
+++ js/elements.js
@@ -6,6 +6,6 @@ export const formRequest = document.querySelector('.form--request');
 export const modalContainer = document.querySelector('.modal__container');
 export const modalSuccess = document.querySelector('.modal__success');
 export const form = document.getElementById('form');
-// export const request = modalElem.querySelector('.modal__order');
+export const request = modalElem.querySelector('.modal__order');
 export const burger = document.querySelector('.burger');
 export const navigation = document.querySelector('.main-nav__opened')
--- js/toggleMenu.js
+++ js/toggleMenu.js
@@ -1,20 +1,3 @@
-// export const toggleMenu = () => {
-//   const navMain = document.querySelector('.main-nav');
-//   const navToggle = document.querySelector('.main-nav__toggle');
-
-//   navMain.classList.remove('main-nav--no-js');
-
-//   navToggle.addEventListener('click', () => {
-//     if (navMain.classList.contains('main-nav--closed')) {
-//       navMain.classList.remove('main-nav--closed');
-//       navMain.classList.add('main-nav--opened');
-//     } else {
-//       navMain.classList.add('main-nav--closed');
-//       navMain.classList.remove('main-nav--opened');
-//     }
-//   });
-
-// }
 
 import { burger, navigation } from "./elements.js";
 
